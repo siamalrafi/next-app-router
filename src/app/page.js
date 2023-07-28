7a78712b4c14bc79f799d1ad17c2150cd2036c4b
siamalrafi <rahathasandipto@gmail.com>
2023-07-28 17:31:02 +0600

    this is the new code

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,8 @@
 "use client";
 export default function Home() {
-   console.log("object");
    return (
       <>
          <h2>Home page</h2>
-         <button className="btn">Button</button>
-         <button className="btn">Button</button>
-         <button className="btn btn-neutral">Neutral</button>
-         <button className="btn btn-primary">Button</button>
-         <button className="btn btn-secondary">Button</button>
-         <button className="btn btn-accent">Button</button>
-         <button className="btn btn-ghost">Button</button>
-         <button className="btn btn-link">Button</button>
       </>
    );
 }
